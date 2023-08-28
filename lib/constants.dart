196e78a78e50905d1366a27541e35ee7d31105b6
sureshksharma <100222829+sureshksharma@users.noreply.github.com>
2023-08-28 22:55:28 +0530

    Update commit

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,5 @@
 
 class Constants {
-  static const appID = 753684684;
-  static const appSign = 'a5be911afca2a2920aa594445ab7d2cb700e67a200b46e693309f910aa6ffc8a';
+  static const appID = <Write your own app ID>;
+  static const appSign = '<Write your own app Sign>';
 }
